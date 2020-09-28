5e2f213b1ac34af1a6851a03f67904defbb838da
Shrikesh <shrikeshs98@gmail.com>
2020-09-28 13:44:14 +0530

    Intial Commit

---
+++ back-end/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+// Mongo db Atlas Dashboard
+
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology: true ,useCreateIndex:true})
+// Second args: jut add 
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log("Mongo Db databse connection is established")
+});
+
+
+
+app.listen(port, (req,res)=> {
+    console.log(`Server is listening at port ${port}`)
+})
